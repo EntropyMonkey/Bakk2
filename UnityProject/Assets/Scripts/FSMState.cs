d47b305001d24e324238c079f100467d3aa32bd2
Andrea Distler <andrea.distler@gmail.com>
2012-04-11 12:01:09 +0200

    - added unity project - added reusable code from BalloonQuest

---
+++ UnityProject/Assets/Scripts/FSMState.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+abstract public class FSMState<T> : ScriptableObject
+{
+	abstract public void Enter( T entity );
+	abstract public void Execute( T entity );
+	abstract public void Exit( T entity );
+}
